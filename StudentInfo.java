b073977e34ace17b6e5ed579e9f369f0fb9801c9
shri8977 <shrisubashini2007@gmail.com>
2025-07-23 06:36:47 +0530

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,23 @@
+import java.io.*;
+class studentInfo {
+    public static void main(String[] args)
+    { // initialise the age and name
+    int age=23;
+    String name="subashini is on fire";
+    String college= "p.a.college";
+    String hobby="coding";
+
+
+        System.out.println("==============================");
+        System.out.println("        java  info            ");
+        System.out.println("==============================");
+        System.out.println("name   :"  +name);
+        System.out.println("age     :" +age);
+        System.out.println("college     :" +college);
+        System.out.println("hobby   :" +hobby);
+        System.out.println("================================");
+        
+        
+
+    }
+}
